447cdbb947362804cb640ce676c067c003c4603e
Zykov Aleksandr <cah34ka@gmail.com>
2022-08-09 12:17:52 +0300

    change Testwork3

--- src/Package1/Testwork3.java
+++ src/Package1/Testwork3.java
@@ -4,6 +4,10 @@ public class Testwork3 {
     public static void main(String[] args) {
 //        Teacher t = new Teacher("Vasya", "Petrov", 454.2);
         System.out.println("a che ne sozdaetsya?");
+        int a =25;
+        int b = 12;
+        int c = a+b;
+        System.out.println(a+b+c);
 
 //    class Teacher {
 //        String name;
