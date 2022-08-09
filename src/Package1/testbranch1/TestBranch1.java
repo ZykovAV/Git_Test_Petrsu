9f12aa25e1b4e481228816c24f44afc2e3296cc9
ZykovAV <cah34ka@gmail.com>
2022-08-09 12:02:41 +0300

    add too much package and classes

---
+++ src/Package1/testbranch1/TestBranch1.java
@@ -0,0 +1,8 @@
+package Package1.testbranch1;
+
+public class TestBranch1 {
+    public static void main(String[] args) {
+        System.out.println("eto Branch1_createIdea");
+        System.out.println("eto branch Master, hmmmm");
+    }
+}
