f11392ea3e66a6ef5cd28dae8fa2405bbcc1a798
ZykovAV <cah34ka@gmail.com>
2022-08-09 12:03:25 +0300

    add too much package and classes

---
+++ src/Package1/TestBranch2_notAdd/TestBranch2.java
@@ -0,0 +1,7 @@
+package Package1.TestBranch2_notAdd;
+
+public class TestBranch2 {
+    public static void main(String[] args) {
+        System.out.println("testBranch2 in idea, not Add");
+    }
+}
