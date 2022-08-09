9c966227eba74373cb142ee91dedf8a7bab610ca
Zykov Aleksandr <cah34ka@gmail.com>
2022-08-09 12:16:15 +0300

    sout testname

--- src/Package1/TestWork1.java
+++ src/Package1/TestWork1.java
@@ -4,5 +4,6 @@ public class TestWork1 {
     public static void main(String[] args) {
         System.out.println("Eto test Work #1");
         System.out.println("metod Main prodoljaet rabotu");
+        System.out.println("test name");
     }
 }
